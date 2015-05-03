d8a4bbe78a22cba2e9c3983711c4c4c22bcd55cb
Alex Kelley <adkelley@mac.com>
2015-05-03 16:53:25 -0700

    Slight refactoring and documentation

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 window.addEventListener("load", function () {
+  /*
+   * myBoard holds 3 rows of original board, plus 3 rows of inverted cols,
+   * plus 2 rows of inverted diagonals.  This makes checking for winner
+   * easy by scanning through 8 rows for contiguous boxes of three 0 or X's
+  */
   var myBoard = new Array(24); // holds O = 0, X = 1, or empty = -1
-  var x, XScore = 0, OScore = 0, tieScore = 0; // 0 = O, 1 = X
+  var x; // Keep track of turn 0 = 0, 1 = X
+  var XScore = 0, OScore = 0, tieScore = 0; // keeps track of score
 
   function updateScoreBoard(whoWon) {
     elem = document.querySelectorAll("li");
@@ -54,10 +60,10 @@ window.addEventListener("load", function () {
       }
     }
       
-    var result = isWinner();
-    if (result !== "continue") {
-      updateScoreBoard(result);
-      alert("Game Over, " + result + "!");
+    var gameOver = isWinner();
+    if (gameOver !== "continue") {
+      updateScoreBoard(gameOver);
+      alert("Game Over, " + gameOver + "!");
       if (confirm("Play again?")) {
         newGame();
       }
@@ -67,26 +73,25 @@ window.addEventListener("load", function () {
 
   function markBox(id) {
     var index = parseInt(id.match(/[0-8]/), 10);
-    var elem = document.getElementById(id);
-    var row = Math.floor(index / 3);
-    var col = Math.floor(index % 3);
-    var num;
     if (myBoard[index] === -1) {
+      var elem = document.getElementById(id);
       elem.innerHTML = (x) ? "X" : "O";
-      num = (x) ? 1 : 0;
-      myBoard[index] =  myBoard[col*3+row+9] = num; // update columns & rows
+      var num = (x) ? 1 : 0;
+      var row = Math.floor(index / 3);
+      var col = Math.floor(index % 3);
+      myBoard[index] =  myBoard[col*3+row+9] = num; // update cols, rows
       if (Math.floor(index % 2) === 0) { // is token on diagonal box?
         if (index === 0) {
-          myBoard[18] = num; // top left corner
+          myBoard[18] = num; // top left corner elem
         }
         else if (index === 4) {
-          myBoard[19] = myBoard[22] = num;  // center box
+          myBoard[19] = myBoard[22] = num;  // center box elem
         }
-        else if (Math.floor(index % 4) === 0) { // left diag
+        else if (Math.floor(index % 4) === 0) { // left diag elem
           myBoard[col+18] = num;
         }
         else {
-          myBoard[col+21] = num;  // right diag
+          myBoard[col+21] = num;  // right diag elem
         }
       }
       x = !x;
